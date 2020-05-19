05642d0a79cbd09095d6b950c1a1567ee74b23ec
Adrianalcantar <65567790+Adrianalcantar@users.noreply.github.com>
2020-05-18 18:11:23 -0700

    Lecture on inspecting data using pandas DataFrame

---
+++ 02 Introduction to Pandas/DataFrames/s2_2_inspecting_data2.py
@@ -0,0 +1,44 @@
+import pandas as pd
+df = pd.read_csv('../../sample_data/02 Introduction to pandas/intel.csv')
+
+
+### print df to make sure it works
+### print(df)
+
+
+### how to check data type
+### print(type(df))
+
+
+### How to check dataframe shape
+### print(df.shape)
+
+
+### How to view colum names
+### print(df.columns)
+
+
+### inspect first rows of data
+### print(df.head(10))
+
+
+### Inspect last roes of data
+### print(df.tail(2))
+
+
+### view summary of dataframe(df) info
+### print(df.info())
+
+### view open colum
+###open = df['Open']
+### print(open)
+
+
+###print(open.head()) to view the first rows
+
+### view one or more colums side by side
+### print(df[['Open', 'Close']].head())
+
+
+
+print(df.describe())
